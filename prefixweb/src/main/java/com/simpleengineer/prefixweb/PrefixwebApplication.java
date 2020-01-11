9ca6aa7cbfdf4cb3b192eeac3256e87428934e51
jeetmpatel1 <jeet.mpatel1@gmail.com>
2020-01-10 21:40:53 -0800

    spring boot based service implementation completed

---
+++ prefixweb/src/main/java/com/simpleengineer/prefixweb/PrefixwebApplication.java
@@ -0,0 +1,13 @@
+package com.simpleengineer.prefixweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrefixwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrefixwebApplication.class, args);
+    }
+
+}
---
+++ prefixweb/src/test/java/com/simpleengineer/prefixweb/PrefixwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleengineer.prefixweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrefixwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
